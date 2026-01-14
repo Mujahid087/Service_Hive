c061f680d579910edce7de4432f0d5dc4f849bef
Mujahid087 <mujahidmahedi10@gmail.com>
2026-01-14 09:43:04 +0530

    creating messages

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,3 @@
-// import express from "express"
-// import dotenv from "dotenv" 
-// import connectDB from "./Config/db.js"
-// import cookieParser from "cookie-parser"
-// import authRoutes from "./routes/AuthRoutes.js"
-// import cors from "cors"
-// import gigRoutes from "./routes/gigRoutes.js"
-// import bidroutes from "./routes/bidRoutes.js" 
-
-
-// dotenv.config()
-// connectDB() 
-
-// const app=express() 
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173", // EXACT frontend URL
-//     credentials: true,               // REQUIRED for cookies
-//   })
-// );
-
-// app.use("/api/auth",authRoutes) 
-// app.use("/api/bid",bidroutes)
-// app.use("/api/gig",gigRoutes)
-
-
-// const PORT=process.env.PORT || 5000 
-// app.listen(PORT,()=> console.log(`server running on port ${PORT}`)) 
-
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./Config/db.js";
